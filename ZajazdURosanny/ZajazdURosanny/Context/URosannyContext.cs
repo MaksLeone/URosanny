af8d9ff03a942c7dd5d3a482549c156c24c59a60
Maks Leone <maksymilian.leone@smith-nephew.com>
2019-10-28 20:53:50 +0100

    Page Template

---
+++ ZajazdURosanny/ZajazdURosanny/Context/URosannyContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZajazdURosanny.Models.Db;
+
+namespace ZajazdURosanny.Context
+{
+    public class URosannyContext : DbContext
+    {
+        public URosannyContext(DbContextOptions<URosannyContext> options) : base(options)
+        {
+
+        }
+    }
+}
